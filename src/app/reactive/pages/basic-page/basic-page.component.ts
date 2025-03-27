43b12bac42fe179d09b0664d5e8ebc5a6c27bbe6
Juan Manuel Arnaiz Caballero <jmanuelarnaiz@gmail.com>
2025-03-27 12:46:22 +0100

    Basic page component

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,51 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './basic-page.component.html',
 })
-export class BasicPageComponent {}
+export class BasicPageComponent {
+  private _fb = inject(FormBuilder);
+  formUtils = FormUtils;
+
+  myForm: FormGroup = this._fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(5)]],
+    inStorage: [0, [Validators.required, Validators.min(1)]],
+  });
+  // Esta forma es muy tediosa, sobre todo si hay varios niveles
+  // de identación. Imagina el caso de que hay una dirección con
+  // varios campos (nombre calle, número, puerta, etc); habría que
+  // crear otro FormGroup dentro de este, lo que sería ya bastante
+  // jaleo.
+  // myForm = new FormGroup({
+  //   name: new FormControl(''),
+  //   price: new FormControl(0),
+  //   inStorage: new FormControl(0),
+  // });
+
+  onSubmit() {
+    if (this.myForm.invalid) {
+      // "Toca" todos los campos del formulario
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    // Reset. Si se llama sin objeto, pone todos los valores a null.
+    // Con un objeto, se le pueden pasar valores con los que se resetea
+    // this.myForm.reset();
+    this.myForm.reset({
+      price: 0,
+      inStorage: 0,
+    });
+  }
+}
