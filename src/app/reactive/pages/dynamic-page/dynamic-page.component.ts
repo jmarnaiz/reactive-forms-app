588317863a4c56671b0f6aa16222178cbf04243a
Juan Manuel Arnaiz Caballero <jmanuelarnaiz@gmail.com>
2025-03-31 13:43:33 +0200

    Dynamic Page Component

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,9 +1,64 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
+const MIN_GAMES = 3;
 @Component({
   selector: 'app-dynamic-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './dynamic-page.component.html',
 })
-export class DynamicPageComponent {}
+export class DynamicPageComponent {
+  private _fb: FormBuilder = inject(FormBuilder);
+  minGames = MIN_GAMES;
+  formUtils = FormUtils;
+
+  myForm: FormGroup = this._fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favoriteGames: this._fb.array(
+      [
+        ['Metal Gear', [Validators.required, Validators.minLength(3)]],
+        [
+          'Final Fantasy VII Remake',
+          [Validators.required, Validators.minLength(3)],
+        ],
+      ],
+      Validators.minLength(this.minGames)
+      // Validador no a los campos, si no al array. Con esto
+      // establezco que debe tener al menos 3 valores
+    ),
+  });
+
+  // newFavoriteControl = this._fb.control([])
+  newFavoriteControl = new FormControl('', Validators.required);
+
+  get favoriteGames(): FormArray {
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
+
+  addToFavorites() {
+    if (this.newFavoriteControl.invalid) return;
+
+    const newGame = this.newFavoriteControl.value;
+    this.favoriteGames.push(this._fb.control(newGame, Validators.required));
+    // this.favoriteGames.push(this.newFavoriteControl);
+
+    this.newFavoriteControl.reset();
+  }
+
+  deleteFavorite(index: number) {
+    this.favoriteGames.removeAt(index);
+  }
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
+}
--- src/app/utils/form-utils.ts
+++ src/app/utils/form-utils.ts
@@ -1,8 +1,14 @@
-import { FormGroup } from '@angular/forms';
+import { FormArray, FormGroup, ValidationErrors } from '@angular/forms';
 
 export class FormUtils {
   static isValidField(form: FormGroup, fieldName: string): boolean | null {
     return form.controls[fieldName].errors && form.controls[fieldName].touched;
+  }
+
+  static isValidFieldInArray(formArray: FormArray, index: number) {
+    return (
+      formArray.controls[index].errors && formArray.controls[index].touched
+    );
   }
 
   static getFieldError(form: FormGroup, fieldName: string): string | null {
@@ -10,6 +16,18 @@ export class FormUtils {
 
     const errors = form.controls[fieldName].errors ?? {};
 
+    return FormUtils._getTextErrors(errors);
+  }
+
+  static getFieldErrorInArray(form: FormArray, index: number): string | null {
+    if (!form.controls.length) return null;
+
+    const errors = form.controls[index].errors ?? {};
+
+    return FormUtils._getTextErrors(errors);
+  }
+
+  private static _getTextErrors(errors: ValidationErrors) {
     for (const key of Object.keys(errors)) {
       switch (key) {
         case 'required':
