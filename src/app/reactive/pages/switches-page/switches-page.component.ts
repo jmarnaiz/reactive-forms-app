b4d01aca92b8f37cf19880b96cefce54d96344a8
Juan Manuel Arnaiz Caballero <jmanuelarnaiz@gmail.com>
2025-04-01 10:38:46 +0200

    Switches Page Component

--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,9 +1,31 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-switches-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './switches-page.component.html',
 })
-export class SwitchesPageComponent {}
+export class SwitchesPageComponent {
+  private _fb = inject(FormBuilder);
+  formUtils = FormUtils;
+
+  myForm: FormGroup = this._fb.group({
+    gender: ['M', Validators.required],
+    wantNotifications: [true],
+    termsAndConditions: [false, Validators.requiredTrue],
+  });
+
+  // Con CTRL + '.' en el template me da la opción
+  // de implementar el método en el controlador
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
+}
