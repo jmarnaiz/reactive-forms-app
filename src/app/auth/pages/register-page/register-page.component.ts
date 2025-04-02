0727ecb2887551ce281bb4697246acc2bacc96a2
Juan Manuel Arnaiz Caballero <jmanuelarnaiz@gmail.com>
2025-04-02 11:47:24 +0200

    Register Page Component

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,9 +1,65 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
+import { CustomFieldComponent } from '../../../shared/components/custom-field/custom-field.component';
 
+const MIN_LENGTH: number = 6;
 @Component({
   selector: 'app-register-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule, CustomFieldComponent],
   templateUrl: './register-page.component.html',
 })
-export class RegisterPageComponent {}
+export class RegisterPageComponent {
+  private _fb = inject(FormBuilder);
+  formUtils = FormUtils;
+
+  myForm = this._fb.group(
+    {
+      name: [
+        '',
+        [Validators.required, Validators.pattern(this.formUtils.namePattern)],
+      ],
+      // Valor por defecto, Validaciones síncronas, Validaciones asíncronas
+      email: [
+        '',
+        [Validators.required, Validators.pattern(this.formUtils.emailPattern)],
+        [this.formUtils.checkServerResponse],
+      ],
+      username: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(MIN_LENGTH),
+          Validators.pattern(this.formUtils.notOnlySpacesPattern),
+          this.formUtils.checkUserName,
+        ],
+        ,
+      ],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(MIN_LENGTH),
+          Validators.pattern(this.formUtils.numberPattern),
+          Validators.pattern(this.formUtils.capitalCasePattern),
+          Validators.pattern(this.formUtils.smallCasePattern),
+          Validators.pattern(this.formUtils.specialCharacterPattern),
+        ],
+      ],
+      password2: ['', Validators.required],
+    },
+    {
+      validators: [this.formUtils.areEqualFields('password', 'password2')],
+    }
+  );
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
+}
+
+/**
+ * Las validaciones síncronas siempre se ejecutan antes que las asíncronas,
+ * lo cual es maravilloso
+ */
--- src/app/utils/form-utils.ts
+++ src/app/utils/form-utils.ts
@@ -1,6 +1,33 @@
-import { FormArray, FormGroup, ValidationErrors } from '@angular/forms';
+import {
+  AbstractControl,
+  FormArray,
+  FormGroup,
+  ValidationErrors,
+} from '@angular/forms';
+
+// Función para simular la petición a un servidor
+async function sleep() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(true);
+    }, 2500);
+  });
+}
 
 export class FormUtils {
+  // Regular expressions
+  static namePattern = /^([a-zA-Z]+) ([a-zA-Z]+)$/;
+  static emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  static notOnlySpacesPattern = /^[a-zA-Z0-9]+$/;
+
+  // Use for passwords
+  static numberPattern = /\d/; // check whether the entered password has a number
+  static capitalCasePattern = /[A-Z]/; // check whether the entered password has upper case letter
+  static smallCasePattern = /[a-z]/; //  check whether the entered password has a lower-case letter
+  static specialCharacterPattern = /[ !@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/; // check whether the entered password has a special character
+
+  static securePasswordPattern: '/^(?=.*[a-z])(?=.*[A-Z])(?=.*d)(?=.*[@$!%*?&])[A-Za-zd@$!%*?&]{8,}$/';
+
   static isValidField(form: FormGroup, fieldName: string): boolean | null {
     return form.controls[fieldName].errors && form.controls[fieldName].touched;
   }
@@ -32,13 +59,72 @@ export class FormUtils {
       switch (key) {
         case 'required':
           return 'Este campo es requerido';
+        case 'email':
+          return 'El email introducido no es válido';
+        case 'emailTaken':
+          return 'El email ya ha sido registrado anteriormente';
         case 'minlength':
           return `Mínimo de ${errors['minlength'].requiredLength} caracteres`;
         case 'min':
           return `Mínimo de ${errors['min'].min} caracteres`;
+        case 'userNameNotValid':
+          return 'Nombre de usuario no permitido';
+        case 'pattern': {
+          const requiredPattern = errors['pattern'].requiredPattern;
+          if (requiredPattern == FormUtils.emailPattern) {
+            return 'Formato de correo electrónico incorrecto';
+          }
+          if (requiredPattern == FormUtils.namePattern) {
+            return 'El formato debe ser nombre y apellido';
+          }
+          if (requiredPattern == FormUtils.notOnlySpacesPattern) {
+            return 'No se permiten espacios';
+          }
+          if (requiredPattern == FormUtils.numberPattern) {
+            return 'Debe contener al menos un número';
+          }
+          if (requiredPattern == FormUtils.capitalCasePattern) {
+            return 'Debe contener al menos un carácter en mayúsculas';
+          }
+          if (requiredPattern == FormUtils.smallCasePattern) {
+            return 'Debe contener al menos un carácter en minúsculas';
+          }
+          if (requiredPattern == FormUtils.specialCharacterPattern) {
+            return 'Debe contener al menos un carácter especial';
+          }
+          return 'Formato incorrecto';
+        }
+        default:
+          return `Error de validación no controlado ${key}`;
       }
     }
 
     return null;
+  }
+
+  static areEqualFields(field1: string, field2: string) {
+    return (group: FormGroup) => {
+      const field1Value = group.controls[field1].value;
+      const field2Value = group.controls[field2].value;
+      return field1Value === field2Value ? null : { notEquals: true };
+    };
+  }
+
+  static async checkServerResponse(
+    control: AbstractControl
+  ): Promise<ValidationErrors | null> {
+    await sleep();
+    const formValue = control.value;
+
+    if (formValue === 'soyjuanman@gmail.com') {
+      return { emailTaken: true };
+    }
+    return null;
+  }
+
+  static checkUserName(control: AbstractControl): ValidationErrors | null {
+    const formValue = control.value;
+
+    return formValue === 'juanmasevillista' ? { userNameNotValid: true } : null;
   }
 }
